907f81ecbf0b01c0e03bffa33ee72f6d5595f597
win-laptop-T460s <1099255210@qq.com>
2021-04-22 08:59:35 +0800

    update md

--- nonebot/bot-03/awesome/plugins/copy.py
+++ nonebot/bot-03/awesome/plugins/copy.py
@@ -4,21 +4,16 @@ import aiofiles
 
 bot = get_bot()
 
-# @on_command('确实',aliases='[CQ:face,id=277]', only_to_me=False)
-# async def weather(session: CommandSession):
-#     weather_report = '确实'
-#     await session.send(weather_report)
-
-
 @bot.on_message("group")
 async def re(context):
     msg = context["message"]
     s_msg = str(msg)
-    # grp = context["group_id"]
+    grp = context["group_id"]
     qq = context["user_id"]
     is_msg_match = await match(s_msg)
     if (is_msg_match):
-        await bot.send_private_msg(user_id=qq,message=s_msg)
+        await bot.send_group_msg(group_id=grp,message=s_msg)
+        # await bot.send_private_msg(user_id=qq,message=s_msg)
 
 
 async def match(str):
